fc05fada3c518ba66a39a1285490992962b912bc
fuel4school <fuel4schoolnyc@gmail.com>
2025-06-27 20:45:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let lastScroll = 0;
+const navbar = document.querySelector('.navbar');
 
+window.addEventListener('scroll', () => {
+  let currentScroll = window.pageYOffset;
+  if (currentScroll > lastScroll && currentScroll > 50) {
+    navbar.classList.add('hidden');
+  } else {
+    navbar.classList.remove('hidden');
+  }
+  lastScroll = currentScroll;
+});
